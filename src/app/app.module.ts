c99fb8dab5cf0dc77357b08a374fa64b89b3239a
kamel <kamelyakhlef@hotmail.fr>
2021-06-14 17:17:28 +0200

    create geolocation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,19 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+// geolocation and native-geocoder
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { NativeGeocoder } from '@ionic-native/native-geocoder/ngx';
+
+import { LaunchNavigator } from "@ionic-native/launch-navigator/ngx";
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, Geolocation,
+    LaunchNavigator,
+    NativeGeocoder,],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,55 @@
 import { Component } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+const { Geolocation } = Plugins;
+import {
+  NativeGeocoder,
+  NativeGeocoderResult,
+  NativeGeocoderOptions,
+} from '@ionic-native/native-geocoder/ngx';
 
-@Component({
+import { LaunchNavigator, LaunchNavigatorOptions } from '@ionic-native/launch-navigator/ngx';@Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+  coords: any;
+  address: any;
+  destAdd = "Paris, FR"
+  constructor(private nativeGeocoder: NativeGeocoder, private launchNavigator: LaunchNavigator) { }
+
+  async locate() {
+    const coordinates = await Geolocation.getCurrentPosition();
+    console.log('Current', coordinates);
+    this.coords = coordinates.coords;
+    console.log(this.coords.latitude.toString() + ', ' + this.coords.longitude.toString());
+    
+  }
+
+  async reverseGeocode() {
+    if (!this.coords) {
+      const coordinates = await Geolocation.getCurrentPosition();
+      this.coords = coordinates.coords;
+    }
+    let options: NativeGeocoderOptions = {
+      useLocale: true,
+      maxResults: 5,
+    };
+    this.nativeGeocoder
+      .reverseGeocode(this.coords.latitude, this.coords.longitude, options)
+      .then((result: NativeGeocoderResult[]) => {
+        this.address = result[0];
+        console.log(result);
+      })
+      .catch((error: any) => console.log(error));
+  }
+
+  
+  naveMe(address){
+    this.launchNavigator.navigate(this.destAdd, {
+      start: address
+  });
+  }
 
 }
